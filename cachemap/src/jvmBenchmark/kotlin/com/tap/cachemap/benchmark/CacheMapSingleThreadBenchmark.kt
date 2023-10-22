a1a8cc7c972951d2a0990ad4b151206e0a48a8b5
CharlieTap <charlie.tapping@deliveroo.co.uk>
2023-10-22 12:47:07 +0100

    fmt

--- cachemap/src/jvmBenchmark/kotlin/com/tap/cachemap/benchmark/CacheMapSingleThreadBenchmark.kt
+++ cachemap/src/jvmBenchmark/kotlin/com/tap/cachemap/benchmark/CacheMapSingleThreadBenchmark.kt
@@ -1,8 +1,18 @@
 package com.tap.cachemap.benchmark
 import com.tap.cachemap.cacheMapOf
-import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Fork
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.TearDown
+import org.openjdk.jmh.annotations.Warmup
 import org.openjdk.jmh.infra.Blackhole
-import java.util.concurrent.*
+import java.util.concurrent.TimeUnit
 
 @State(Scope.Benchmark)
 @Fork(value = BenchmarkConfig.FORKS)
