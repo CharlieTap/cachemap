5c71ecd61808ca1ab5685c7916d312f2bfd5327c
CharlieTap <charlie.tapping@deliveroo.co.uk>
2023-12-23 17:20:43 +0000

    remove explicit tracking of .editorconfig

--- gradle/plugins/linting-conventions/src/main/kotlin/linting-conventions.gradle.kts
+++ gradle/plugins/linting-conventions/src/main/kotlin/linting-conventions.gradle.kts
@@ -4,9 +4,6 @@ plugins {
     id("org.jmailen.kotlinter")
 }
 
-val config = rootProject.file(".editorconfig")
-
 tasks.withType<ConfigurableKtLintTask>().configureEach {
-    inputs.file(config).withPathSensitivity(PathSensitivity.RELATIVE)
     exclude { it.file.path.contains("build/")}
 }
