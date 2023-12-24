1975747a9c5bd511e69bf3a7f9cb4c4129e2973e
CharlieTap <charlie.tapping@deliveroo.co.uk>
2023-12-24 15:19:34 +0000

    kotlin 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.atomic.fu) apply false
     alias(libs.plugins.dokka) apply false
     id("maven-publish")
+    id("versions-conventions")
 }
 
 tasks.register("clean",Delete::class){
